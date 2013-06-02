41f71b790577a26b6c797bc09107b9981064252b
Manuel Hoffmann <manuel@polythematik.de>
2013-06-02 17:07:55 +0200

    Added front page animation and loooong breadth first search run

---
+++ theme.js
@@ -0,0 +1,36 @@
+
+$(function() {
+	function getRandomColor() {
+		var max = 0x111111;
+		var rnd = Math.floor(Math.random() * max);
+		var rnd = 0x252541 + rnd
+		return '#' + rnd.toString(16);
+	}
+
+	var sigRoot = document.getElementById('sigma-title');
+	var sigInst = sigma.init(sigRoot).drawingProperties({
+		defaultLabelColor: getRandomColor(),
+		defaultLabelSize: 14,
+		defaultLabelBGColor: getRandomColor(),
+		defaultLabelHoverColor: getRandomColor(),
+		labelThreshold: 6,
+		defaultEdgeType: 'curve' 
+	}).graphProperties({
+		minNodeSize: 0.5,
+		maxNodeSize: 5,
+		minEdgeSize: 1,
+		maxEdgeSize: 1
+	}).mouseProperties({
+		maxRatio: 32
+	});
+
+	sigInst.parseGexf('data.gexf');
+	sigInst.iterNodes(function(a) { a.color=getRandomColor(); })
+
+	sigInst.resize();
+	sigInst.draw();
+	sigInst.startForceAtlas2();
+	window.setTimeout(function() {
+		sigInst.stopForceAtlas2();
+	},10000)
+})
